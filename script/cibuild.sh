31ce8bf1c8bbc540ea78c266940041e348571230
JbIPS <jeanbaptiste.richardet@gmail.com>
2017-03-13 18:07:04 +0100

    Automatically update by Travis

--- script/cibuild.sh
+++ script/cibuild.sh
@@ -3,3 +3,15 @@ set -e # halt script on error
 
 bundle exec jekyll build
 #bundle exec htmlproofer ./docs
+
+git config --global user.email "jeanbaptiste.richardet@gmail.com"
+git config --global user.name "JbIPS (Travis Bot)"
+
+git branch -a
+git checkout master
+git remote -v
+git status
+git add docs
+git commit -m 'Updates generated site'
+git status
+git push https://$GH_TOKEN@github.com/ARPHD/ARPHD.git master
